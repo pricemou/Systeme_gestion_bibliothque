1daab0e2a1f462a19d8352681193f74393152d84
Cherlie Rivage <crivage27@gmail.com>
2024-04-02 16:51:12 -0400

    I commit

--- src/Livre.java
+++ src/Livre.java
@@ -2,17 +2,59 @@
 
 // Définition de la classe Livre
 class Livre {
-    int id;
-    String titre;
-    String auteur;
-    String ISBN;
-    boolean disponible;
+  // private  int id; -- Je pense qu'on peut utiliser l'ISBN pour identifier de façon unique chaque livre pas besoin d'un identifiant
+    private String titre;
+   private  String auteur;
+    private String ISBN;
+   private  boolean disponible;
 
     public Livre(int id, String titre, String auteur, String ISBN) {
-        this.id = id;
+       // this.id = id;
         this.titre = titre;
         this.auteur = auteur;
         this.ISBN = ISBN;
         this.disponible = true;
     }
+// Les accesseurs et mutateurs
+  /*  public int getId() {
+        return id;
+    }
+    */
+
+    public String getTitre(){
+        return titre;
+    }
+    public String getAuteur(){
+        return auteur;
+    }
+    public String getISBN(){
+        return ISBN;
+    }
+
+    /* public boolean isDisponible() {
+       // return disponible;
+    }
+    */
+
+/*    public void setId(int id) {
+        this.id = id;
+    }
+*/
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+   /* public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    */
+
 }
---
+++ src/SystemeGestionBibliotheque.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class SystemeGestionBibliotheque {
+
+public static void main(String[]args){
+   Scanner sc = new Scanner(System.in);
+   int choix;
+   do{
+      System.out.println("\nMenu:");
+      System.out.println("1. Ajouter un livre");
+      System.out.println("2. Rechercher un livre");
+      System.out.println("3. Emprunter un livre");
+      System.out.println("4. Retourner un livre");
+      System.out.println("0. Quitter");
+      System.out.println("Votre choix:");
+      choix = sc.nextInt();
+      sc.nextLine();  // Consommer la nouvelle ligne
+
+       if (choix == 1) {
+           System.out.println("Saisir le titre du livre");
+           String titre = sc.nextLine();
+           System.out.println("Saisir le nom de l'auteur");
+           String auteur = sc.nextLine();
+           System.out.println("Saisir l'ISBN du livre");
+           String ISBN = sc.nextLine();
+       }
+   }while (choix !=0);
+   sc.close(); // Fermeture de la classe scanner
+}
+
+}
